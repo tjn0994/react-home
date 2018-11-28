34e08b246ddff302b928433bc18ca531c22213a4
tin <quangtin12b5@gmail.com>
2018-11-28 09:09:28 +0700

    refactor code

--- src/components/Users/FormUserComponent.js
+++ src/components/Users/FormUserComponent.js
@@ -2,41 +2,28 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-import history from '../../services/RoutingService';
-import { updateUser, createUser } from '../../actions/userAction';
 import { selector } from '../../selectors/userSelector';
 
-class FormUserComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
+const FormUserComponent = (props) => {
+  const { values, errors, handleChange, handleSubmit, touched } = props;
 
-  render(){
-    const {
-      values,
-      errors,
-      handleChange,
-      handleSubmit,
-      touched,
-    } = this.props;
-    return (
-      <div className='container' style={{ paddingTop: 50, paddingBottom: 50, minHeight: 600 }}>
-        <Form onSubmit={handleSubmit}>
-          <FormGroup>
-            <Label for='name'>Name</Label>
-            <Input type='text' name='name' id='name' value={this.props.values["name"] || ''} placeholder='...' onChange={handleChange} />
-          </FormGroup>
-          <div>{touched.name && errors["name"]}</div>
-          <FormGroup>
-            <Label for='age'>Age</Label>
-            <Input type='number' name='age' id='age' value={this.props.values["age"] || ''} placeholder='...' onChange={handleChange} />
-          </FormGroup>
-          <div>{touched.age && errors["age"]}</div>
-          <Button type="submit" >Submit</Button>
-        </Form>
-      </div>
-    );
-  }
+  return (
+    <div className='container' style={{ paddingTop: 50, paddingBottom: 50, minHeight: 600 }}>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label for='name'>Name</Label>
+          <Input type='text' name='name' id='name' value={values["name"] || ''} placeholder='...' onChange={handleChange} />
+        </FormGroup>
+        <div>{touched.name && errors["name"]}</div>
+        <FormGroup>
+          <Label for='age'>Age</Label>
+          <Input type='number' name='age' id='age' value={values["age"] || ''} placeholder='...' onChange={handleChange} />
+        </FormGroup>
+        <div>{touched.age && errors["age"]}</div>
+        <Button type="submit" >Submit</Button>
+      </Form>
+    </div>
+  );
 }
 
-export default FormUserComponent;
+export {FormUserComponent};
--- src/containers/User/Form/index.js
+++ src/containers/User/Form/index.js
@@ -3,40 +3,77 @@ import { compose } from 'recompose';
 import { withFormik } from 'formik';
 
 import { selector } from '../../../selectors/userSelector';
-import FormUserComponent from '../../../components/Users/FormUserComponent';
+import {FormUserComponent} from '../../../components/Users/FormUserComponent';
 import { createUser, updateUser } from '../../../actions/userAction';
 import validation from './validation';
 
-const FormikFormUser = withFormik({
-  displayName: 'User Form',
-  mapPropsToValues: (props: Props) => {
-    const user = props.users.find((user) => user.id === parseInt(props.match.params["id"]));
-    if (user) {
-      return (
-        {
-          id: user["id"] || '',
-          name: user["name"] || '',
-          age: user["age"] || '',
-        }
-      );
-    } else {
-      return ({ id: '', name: '', age: '' });
-    }
-  },
-  validationSchema: validation,
-  handleSubmit: (values, option) => {
-    const {users, create, update, history } = option.props;
-    if(values["id"]) {
-      let index = users.findIndex((user) => user.id === values["id"]);
-      users.splice(index, 1, {id: values["id"], name: values["name"], age: values["age"]});
-      update({ users: users });
-    } else {
-      let id_new = (users.length === 0 ? 1 : (users[users.length - 1]["id"] + 1));
-      users.push({id: id_new, name: values["name"], age: values["age"]})
-      create({ users: users });
-    }
-    history.replace('/');
-  },
-})(FormUserComponent);
+// Ex1:
+// const FormikFormUser = withFormik({
+//   displayName: 'User Form',
+//   mapPropsToValues: (props: Props) => {
+//     const user = props.users.find((user) => user.id === parseInt(props.match.params["id"]));
+//     if (user) {
+//       return (
+//         {
+//           id: user["id"] || '',
+//           name: user["name"] || '',
+//           age: user["age"] || '',
+//         }
+//       );
+//     } else {
+//       return ({ id: '', name: '', age: '' });
+//     }
+//   },
+//   validationSchema: validation,
+//   handleSubmit: (values, option) => {
+//     const {users, create, update, history } = option.props;
+//     if(values["id"]) {
+//       let index = users.findIndex((user) => user.id === values["id"]);
+//       users.splice(index, 1, {id: values["id"], name: values["name"], age: values["age"]});
+//       updateUser({ users: users });
+//     } else {
+//       let id_new = (users.length === 0 ? 1 : (users[users.length - 1]["id"] + 1));
+//       users.push({id: id_new, name: values["name"], age: values["age"]})
+//       createUser({ users: users });
+//     }
+//     history.replace('/');
+//   },
+// })(FormUserComponent);
+//
+// export default connect(selector, {updateUser, createUser})(FormikFormUser);
 
-export default connect(selector, {update: updateUser, create: createUser})(FormikFormUser);
+// Ex2:
+export default compose(
+  connect(selector, { updateUser, createUser }),
+  withFormik({
+    displayName: 'User Form',
+    mapPropsToValues: (props: Props) => {
+      const user = props.users.find((user) => user.id === parseInt(props.match.params["id"]));
+      if (user) {
+        return (
+          {
+            id: user["id"] || '',
+            name: user["name"] || '',
+            age: user["age"] || '',
+          }
+        );
+      } else {
+        return ({ id: '', name: '', age: '' });
+      }
+    },
+    validationSchema: validation,
+    handleSubmit: (values, option) => {
+      const {users, create, update, history } = option.props;
+      if (values["id"]) {
+        let index = users.findIndex((user) => user.id === values["id"]);
+        users.splice(index, 1, {id: values["id"], name: values["name"], age: values["age"]});
+        updateUser({ users: users });
+      } else {
+        let id_new = (users.length === 0 ? 1 : (users[users.length - 1]["id"] + 1));
+        users.push({id: id_new, name: values["name"], age: values["age"]})
+        createUser({ users: users });
+      }
+      history.replace('/');
+    },
+  }),
+)(FormUserComponent);
