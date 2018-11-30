36e0931399b83c12c245ca72e82b94459ed6a87b
tin <quangtin12b5@gmail.com>
2018-11-30 14:03:19 +0700

    Use redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,32 @@
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer, createTransform } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 import rootReducer from '../reducers/rootReducer';
 import rootSaga from '../sagas/rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
+export const PERSIST_KEY = 'root';
+const persistConfig = {
+  key: PERSIST_KEY,
+  storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ['user'],
+  transforms: [createTransform(
+    (inboundState) => ({ ...inboundState, error: '' }),
+    (outboundState) => ({ ...outboundState, error: '' }),
+    { whitelist: ['user'] }
+  )],
+};
 
-export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const configDevStore = () => createStore(
+  persistReducer(persistConfig, rootReducer),
+  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose)(applyMiddleware(sagaMiddleware)),
+);
+
+export const store = configDevStore();
+export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
